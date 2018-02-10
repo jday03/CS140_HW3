d9920f87269df7e14e29e05a81d4dd617d18e8c8
noelvargas <noelleon216@gmail.com>
2018-02-09 22:01:28 -0800

    gennbody() completed (not tested yet ) readnbody() finished needs MPI

--- submit.c
+++ submit.c
@@ -12,39 +12,131 @@ void readnbody(double** s, double** v, double* m, int n) {
 	int i;
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-
+	int size = n / nprocs;
 	// Open the input file
 	FILE * fp;
 	fp = fopen ("./input.txt", "r");
 	if (fp == NULL) {
 	  fprintf(stderr, "error, cannot find input file");
 	}
+
+
+	tempS = (double **)malloc(sizeof(double *) * size);
+	for (i = 0; i < size; i++) {
+		tempS[i] = (double*)malloc(sizeof(double) * 3);
+		for(j = 0; j < 3; j++) {
+			tempS[i][j] = 0;
+		}
+	}
+
+	TempV = (double **)malloc(sizeof(double *) * size);
+	for (i = 0; i < size; i++) {
+		TempV[i] = (double*)malloc(sizeof(double) * 3);
+		for(j = 0; j < 3; j++) {
+			TempV[i][j] = 0;
+		}
+	}
+
+	TempM = (double *)malloc(sizeof(double) * size);
+
+	for(i = 0; i < size; i++) {
+		m[i] = 0;
+	}
 	
-	// This is an example of reading the body parameters from the input file. 
 	if (myrank == 0) {
-		for (i = 0; i < n; i++) {
+		int i;
+		for (i = 0; i < nprocs; i++){
 			double x, y, z, vx, vy, vz, mass;
 
-			int result = fscanf(fp, INPUT_BODY, &x, &y, &z, &vx, &vy, &vz, &mass);
 			if (result != 7) {
 				fprintf(stderr, "error reading body %d. Check if the number of bodies is correct.\n", i);
 				exit(0);
 			}
-			s[i][0] = x;
-			s[i][1] = y;
-			s[i][2] = z;
-			v[i][0] = vx;
-			v[i][1] = vy;
-			v[i][2] = vz;
-			m[i] = mass;
-			
+			if( i == 0 ){
+				int j;
+				for (j = 0; i < size; i++) {
+					int result = fscanf(fp, INPUT_BODY, &x, &y, &z, &vx, &vy, &vz, &mass);
+					if (result != 7) {
+						fprintf(stderr, "error reading body %d. Check if the number of bodies is correct.\n", i);
+						exit(0);
+					}
+
+					s[j][0] = x;
+					s[j][1] = y;
+					s[j][2] = z;
+					v[j][0] = vx;
+					v[j][1] = vy;
+					v[j][2] = vz;
+					m[j] = mass;
+				}
+			}
+			else{ //MPI_Send to the rest of processors using tempS, tempV, tempM
+
+			}
+
 		}
 	}
+
+	else{
+		//MPI Receive Data
+	}
 	fclose(fp);
+
+
 }
 
-void gennbody(double** s, double** v, double* m, int n) {
-	printf("Generate nBody initial condition here.\n");
+void gennbody(double** s, double** v, double* m, int n){
+	//Write This Func:
+	int myrank;
+	int nprocs;
+	int size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+	size = n/nprocs;
+	double massMax = 10^30;
+	double distMax = 0.5 *10 ^13;
+	double dist;
+	double thetaMax = 2 * M_PI;
+	double theta;
+	double zMax = 10 ^11;
+
+
+
+
+	double div;
+
+
+	int i, j;
+	srand(time(NULL));
+	for(i = 0; i < size; i++){
+		//generate random masses from 0 to 10^30
+		div = RAND_MAX / massMax;
+		m[i] = rand() /div;
+		//generate random position and set velocity = 0
+		for(j=0; j < 3; j++){
+			v[i][j] = 0;  //velocity
+
+			// setting positions
+			div = RAND_MAX/ distMax;
+			dist = rand() / div;
+			div = RAND_MAX / thetaMax;
+			theta = rand() / div;
+
+			if(j == 0){//x
+				s[i][j] = dist * cos(theta);
+			}
+			if(j == 1){//y
+				s[i][j] = dist * sin(theta);
+			}
+			if(j == 2){ //z
+				div = RAND_MAX/ zMax;
+				s[i][j] = zMax/2 + (rand() /div );
+			}
+		}
+
+	}
+
 }
 
 void nbody(double** s, double** v, double* m, int n, int iter, int timestep) {
